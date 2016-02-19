50c807eee3cdc30ea4479a00d232dc23ac21e8e2
Zebulun Arendsee <arendsee@iastate.edu>
2016-02-19 00:21:51 -0600

    Commented and refactored

--- hello-world/hw.asm
+++ hello-world/hw.asm
@@ -1,18 +1,38 @@
-; This is a hello world program borrowed from TutorialPoint
+; This is a hello world program adapted from TutorialPoint
 
+; the relative positions of the sections doesn't matter, they both produce
+; identical code
+
+; initialize variables
+section	.data
+msg db 'Hey', 0xa  ;initialize the msg variable
+len equ $ - msg    ;initialize the len variable
+
+; program code
 section	.text
-   global_start   ;must be declared for linker (ld)
-	
+   global_start   ;required in all programs, must be declared for linker (ld)
+
 _start:	          ;tells linker entry point
-   mov	edx,len   ;message length
-   mov	ecx,msg   ;message to write
-   mov	ebx,1     ;file descriptor (stdout)
+
+;OPCODE REGISTER, VAR
+; executable instructions
+; each opcode runs one machine code
+
+
+; 3 arguments for write system call, in C: write(1, msg, len);
+; The order of these declarations don't matter
+; The registers just must be loaded before the kernel call
+; ARGV 0, function name
    mov	eax,4     ;system call number (sys_write)
+; ARGV 1
+   mov	ebx,1     ;file descriptor (stdout)
+; ARGV 2
+   mov	ecx,msg   ;message to write
+; ARGV 3
+   mov	edx,len   ;message length, variables declared in .data section
+; execute whatever is in eax
    int	0x80      ;call kernel
 	
+; set eax to a new command (sys_exit)
    mov	eax,1     ;system call number (sys_exit)
    int	0x80      ;call kernel
-
-section	.data
-msg db 'Hello, world', 0xa  ;our dear string
-len equ $ - msg             ;length of our dear string
