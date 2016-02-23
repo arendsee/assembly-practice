e79ca368cc01bd98d9848f6bd3683d281ef4d9b5
Zebulun Arendsee <arendsee@iastate.edu>
2016-02-22 21:56:05 -0600

    Add simple array

--- basic-memory/mem.asm
+++ basic-memory/mem.asm
@@ -6,6 +6,7 @@
 section	.data
 
 x db 31h
+v db 61h,62h,63h,64h,65h ;abcde
 newline db 0xa
 
 section	.text
@@ -24,6 +25,19 @@ mov ebx, STDOUT
 mov ecx, newline
 ; NOTE: this doesn't work, since 0xa is interpreted as an address
 ; mov ecx, 0xa
+mov edx, 1
+int 0x80
+
+mov eax, SYS_WRITE
+mov ebx, STDOUT
+mov ecx, v
+add ecx, 2 ; move to third element of the array
+mov edx, 2 ; print 3rd and 4th elements
+int 0x80
+
+mov eax, SYS_WRITE
+mov ebx, STDOUT
+mov ecx, newline
 mov edx, 1
 int 0x80
 
